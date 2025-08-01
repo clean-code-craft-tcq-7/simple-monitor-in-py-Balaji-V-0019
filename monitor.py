89aa903812c0f61199333d45e787da86396d2417
lvj1cob <v.balaji3@in.bosch.com>
2025-08-01 11:53:14 +0530

    added more testcases

--- monitor.py
+++ monitor.py
@@ -1,9 +1,17 @@
-
+"""
+Monitor module for checking vital signs.
+"""
 from time import sleep
 import sys
 
 
 def __display_vital_alert(val, msg):
+    """
+    Display an alert message for vital signs.
+    :parm arg: val: Number of seconds to display the alert.
+    :parm arg: msg: Message to display.
+    :return: None
+    """
     print(msg)
     for _ in range(val):
         print('\r* ', end='')
@@ -15,6 +23,14 @@ def __display_vital_alert(val, msg):
 
 
 def __is_vital_ok(name, value, min_val, max_val):
+    """
+    Check if a vital sign is within the acceptable range.
+    :parm arg: name: Name of the vital sign.
+    :parm arg: value: Value of the vital sign.
+    :parm arg: min_val: Minimum acceptable value.
+    :parm arg: max_val: Maximum acceptable value.
+    :return: True if the vital sign is within range, False otherwise.
+    """
     if value < min_val or value > max_val:
         __display_vital_alert(6, f'{name} is out of range!')
         return False
@@ -22,18 +38,40 @@ def __is_vital_ok(name, value, min_val, max_val):
 
 
 def is_temperature_ok(temperature):
+    """
+    Check if the temperature is within the acceptable range.
+    :parm arg: temperature: Temperature value to check.
+    :return: True if the temperature is within range, False otherwise.
+    """
     return __is_vital_ok('Temperature', temperature, 95, 102)
 
 
 def is_pulse_rate_ok(pulse_rate):
+    """
+    Check if the pulse rate is within the acceptable range.
+    :parm arg: pulse_rate: Pulse rate value to check.
+    :return: True if the pulse rate is within range, False otherwise.
+    """
     return __is_vital_ok('Pulse Rate', pulse_rate, 60, 100)
 
 
 def is_spo2_ok(spo2):
+    """
+    Check if the oxygen saturation is within the acceptable range.
+    :parm arg: spo2: Oxygen saturation value to check.
+    :return: True if the oxygen saturation is within range, False otherwise.
+    """
     return __is_vital_ok('Oxygen Saturation', spo2, 90, float('inf'))
 
 
 def vitals_ok(temperature, pulse_rate, spo2):
+    """
+    Check if all vital signs are within the acceptable range.
+    :parm arg: temperature: Temperature value to check.
+    :parm arg: pulse_rate: Pulse rate value to check.
+    :parm arg: spo2: Oxygen saturation value to check.
+    :return: True if all vital signs are within range, False otherwise.
+    """
     return (
       is_temperature_ok(temperature) and
       is_pulse_rate_ok(pulse_rate) and
--- monitor.test.py
+++ monitor.test.py
@@ -1,11 +1,41 @@
+"""
+Test cases for checking the monitor module.
+"""
 import unittest
 from monitor import vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
+    """
+    Test cases for the monitor module.
+    """
     def test_not_ok_when_any_vital_out_of_range(self):
+        """
+        Test that vitals_ok returns False when any vital is out of range.
+        """
         self.assertFalse(vitals_ok(99, 102, 70))
         self.assertTrue(vitals_ok(98.1, 70, 98))
+
+    def test_temperature_out_of_range(self):
+        """
+        Test that vitals_ok returns False when temperature is out of range.
+        """
+        self.assertFalse(vitals_ok(94, 70, 98))
+        self.assertFalse(vitals_ok(103, 70, 98))
+
+    def test_pulse_rate_out_of_range(self):
+        """
+        Test that vitals_ok returns False when pulse rate is out of range.
+        """
+        self.assertFalse(vitals_ok(98.1, 59, 98))
+        self.assertFalse(vitals_ok(98.1, 101, 98))
+
+    def test_spo2_out_of_range(self):
+        """
+        Test that vitals_ok returns False when spo2 is out of range.
+        """
+        self.assertFalse(vitals_ok(98.1, 70, 89))
+        self.assertFalse(vitals_ok(98.1, 70, 101))
 
 
 if __name__ == '__main__':
